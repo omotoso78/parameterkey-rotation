adeb944db65adb69865f75c0b99e2fad2101b09e
omotoso78 <97041293+omotoso78@users.noreply.github.com>
2022-03-14 18:05:53 +0000

    Update ssm-rotation-function.py

--- python-ssm/ssm-rotation-function.py
+++ python-ssm/ssm-rotation-function.py
@@ -1,11 +1,11 @@
+import json
 import boto3
 import string
 import random
-import json
 
 print("loading function")
 
-def lambda_handler(event, contest):
+def lambda_handler(event, context):
     AWS_REGION = "eu-west-3"
     source = string.ascii_letters + string.digits
     key_gen = ''.join((random.choice(source) for i in range(50)))
@@ -14,52 +14,33 @@ def lambda_handler(event, contest):
     
     print('event:', json.dumps(event))
     
-    keyName =json.loads(event['body']['keyName']) #added to define variable 
+   # keyName = json.loads(event['body']['keyName'])
     
-    #Using put method, as it can update existing key values before it is returned, and input new keyname and genrate key_values
-    if ['httpMethod'] == "PUT": 
-        response =client.put_parameter(
+    body = json.loads(event['body'])
+    keyName = body['keyName']#added to define variable keyName
+    #keyName = event['queryStringParameters']['keyName']
+    
+    
+    try:
+        key_value = body['value']
+    except:
+        key_value = str(key_gen)
+   
+   #Using put method, to update existing key values, and input new keyname and generate key_values
+    parameter_detail =client.put_parameter(
                 Name= keyName,
-                #Description='keys',
-                Value= key_gen,
+                Value= key_value,
                 Type='String',
                 Overwrite=True
            )
+    #using get method to return key details
+    parameter_return = client.get_parameter(Name=keyName)
     
-
-    pass
-
-    print(response) # to be formated 
-
-
-
-
-'''
-import json
-import boto3
-import string
-import random
-
-
-source = string.ascii_letters + string.digits
-key_gen = ''.join((random.choice(source) for i in range(50)))
-
-AWS_REGION = "eu-west-3"
-
-def lambda_handler(event, context):
-    
- 
-    client = boto3.client("ssm", region_name = AWS_REGION)
-   
-    response = client.put_parameter(
-            Name= "ParameterName",
-            #Description='keys',
-            Value= str(key_gen),
-            Type='String',
-            Overwrite=True
-           )
-    
-
-      
-    response = {'statusCode': 200, 'body': json.dumps("success") } # to format with variable key name and value
-    return response'''
+    value_body = {
+        "Name": keyName,
+        "Value": key_value
+    }
+    return{
+        "statusCode": 200,
+        "body":json.dumps(value_body)
+    }    
