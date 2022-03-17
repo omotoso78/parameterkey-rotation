bee9ac5114cdc951b9994d35f01b21da05c20765
omotoso78 <97041293+omotoso78@users.noreply.github.com>
2022-03-17 18:55:34 +0000

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,7 +1,7 @@
-# IAM role for Cloudwatch
+# IAM role for Cloudwatch &SSM policy
 
 resource "aws_iam_role" "lambda_exec" {
-  name = "ssmlambda_cloudwatch"
+  name = "ssmlambda"
   assume_role_policy = <<-POLICY
 {
   "Version": "2012-10-17",
@@ -19,12 +19,10 @@ resource "aws_iam_role" "lambda_exec" {
 POLICY
 }
 
-
-##configure inline policy (using data source option to be completed later)
+##configure policy
 
 resource "aws_iam_policy" "ssm_policy" {
   name        = "ssm_lambda_policy"
-  path        = "/"
   description = "MANAGED BY TERRAFORM Allow ssm_lambda to log"
 
   policy = <<POLICY
@@ -33,13 +31,20 @@ resource "aws_iam_policy" "ssm_policy" {
     "Statement": [
         {
           "Effect": "Allow",
-          "Action": ["log:*"],
-          "Resource": "${aws_lambda_function.ssm_lambda.arn}/*"
+          "Action": ["logs:CreateLogStream", "logs:CreateGroup"],
+          "Resource": "arn:aws:logs:*:*:*"
         },
+        
         {
           "Effect": "Allow",
-          "Action": ["ssm:*"],
-          "Resource": "${aws_lambda_function.ssm_lambda.arn}/*"
+          "Action": ["ssm:PutParameter","ssm:GetParameters","ssm:GetParameter","ssm:DeleteParameter","ssm:DeleteParameters","logs:PutLogEvents" ],
+          "Resource": "*"
+        },
+
+        {
+          "Effect": "Allow",
+          "Action": ["ssm:DescribeParameters"],
+          "Resource": "*"
         }
     ]
 }
